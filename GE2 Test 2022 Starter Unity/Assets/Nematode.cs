dea030a48506cf46ba06e913c511aaf900aec01d
MonkeyZebraProductions <73033054+MonkeyZebraProductions@users.noreply.github.com>
2022-04-26 10:12:58 +0100

    First Push

--- GE2 Test 2022 Starter Unity/Assets/Nematode.cs
+++ GE2 Test 2022 Starter Unity/Assets/Nematode.cs
@@ -4,13 +4,34 @@ using UnityEngine;
 
 public class Nematode : MonoBehaviour
 {
-    public int length = 5;
+    public float length = 5;
 
     public Material material;
+
+    public transform StartPos;
+    private Vector3 SphereScale=Vector3.one;
 
     void Awake()
     {
         // Put your code here!
+
+        for(int i=0;i<=Random.Range(1,length), i++)
+        {
+            GameObject sphere= GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            sphere.transform.SetPatent(transform.this)
+            sphere.transform.position = StartPos.position + new Vector3(i, 0, 0);
+            sphere.renderer.material.color = Color.HSVtoRGB(i, 1, 1);
+
+            if(i <= Math Mathf.Ceil(length / 2))
+            { 
+               SphereScale+= new Vector3(1, 0, 0)
+            }
+            else
+            {
+                SphereScale -= new Vector3(1, 0, 0)
+            }
+            sphere.transform.scale = SphereScale;
+         }
     }
 
 
